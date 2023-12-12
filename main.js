59afcde99ce96a9300b89d90df212be0145adc8c
Tural E. Jafarov <Tural.Jafarov@nabgroup.az>
2023-12-12 15:11:15 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+
+// 1-ci task
+
+// let birinci =prompt("Melumati qeyd edin:");
+// let ikinci=birinci.slice(0,1).toUpperCase();
+// console.log(ikinci);
+
+
+// 2-ci task
+
+// let birinci= prompt("Melumati qeyd edin: ");
+// let netice= birinci.slice(0, 1).toUpperCase() + birinci.slice(1);
+// console.log(netice); 
+
+
+// 3-cu task
+
+// let birinci = prompt("Melumati qeyd edin:");
+// if(isNaN(birinci)){
+//     console.log("string");
+// }else{
+//     console.log("Number");
+// };
+
+
+// 4-cu task
+
+// let birinci =prompt("Melumati qeyd edin: ");
+// if(isNaN(birinci)){
+//     let arr=[];
+//     arr.push(birinci);
+//     console.log(arr);
+// }else{
+//     console.log("Numberdir deye add olunmur");
+// };
+
+
+// 5-ci task
+
+// let birinci = prompt("Melumati qeyd edin:");
+// let arr=[];
+// let ikinci =birinci.split("");
+// arr.push(ikinci);
+// console.log(ikinci);
+
+
+
+// 6-ci task
+
+// let birinci = "Baki gozel seher mehriban diyar";
+// let ikinci =birinci.split(' , ')
+// let arr = [ikinci];
+// console.log(arr);
+
+
+
+// 7-ci task
+
+// let birinci ="Baki gozel seher mehriban diyar";
+// let ikinci = birinci.split(" ").join("----");
+// console.log(ikinci);
+
+
+// 8-ci task
+
+// let birinci = prompt("Melumati daxil edin: ");
+// let netice =birinci.charAt(birinci.length-1).toUpperCase();
+// console.log(netice);
+
+
+// 9-cu task
+
+// let birinci =prompt("Melumati daxil edin: ");
+// let netice = birinci.charAt(birinci.length-1).toUpperCase();
+// let birlesdir =netice.concat(birinci);
+// console.log(birlesdir);
