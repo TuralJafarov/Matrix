1d084378c50021e8d4e2c0d2178203390a930665
Tural E. Jafarov <Tural.Jafarov@nabgroup.az>
2023-12-15 17:27:26 +0400

    update

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+   
+
+
+// String metodlari
 
 // 1-ci task
 
@@ -74,3 +78,66 @@
 // let netice = birinci.charAt(birinci.length-1).toUpperCase();
 // let birlesdir =netice.concat(birinci);
 // console.log(birlesdir);
+
+
+
+
+
+
+
+
+
+//? Task click edende sozu tapib bg renglemek
+
+// let input = document.querySelector("input");
+// let button = document.querySelector("button");
+// let words = document.querySelector("p");
+
+
+// button.addEventListener("click", () => {
+//     let searchText = input.value.trim();
+//     words.innerText = searchText;
+//     words.style.color = "red";
+
+//     input.value = "";
+// });
+
+
+
+
+
+
+
+
+//? Task click edende sozu tapib bg renglemek
+
+// let button = document.querySelector("button");
+// let p = document.querySelector("p");
+// let input1 = document.querySelector("#input1");
+// let input2 = document.querySelector("#input2");
+// let input3 = document.querySelector("#input3");
+
+
+
+
+// button.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     let value1 = parseFloat(input1.value);
+//     let value2 = parseFloat(input2.value);
+//     let value3 = parseFloat(input3.value);
+
+//     if (isNaN(value1) || isNaN(value2) || isNaN(value3)) {
+//         p.innerText = "Zehmet olmasa duzgun ededleri daxil edin";
+//     } else {
+//         let minValue = Math.min(value1, value2, value3);
+//         let maxValue = Math.max(value1, value2, value3);
+
+//         p.innerText = "Ən kiçik ədəd: " + minValue + ", Ən böyük ədəd: " + maxValue;
+//     }
+// });
+
+
+
+
+
